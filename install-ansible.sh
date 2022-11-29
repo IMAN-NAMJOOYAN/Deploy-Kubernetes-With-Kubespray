5c4a61bb492f2ac6419ff83c29253697fdea283d
Iman Namjooyan <im.namjuyan@gmail.com>
2022-11-29 13:58:58 +0330

    Add files via upload

---
+++ install-ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip3.8 install virtualenv --user
+VENVDIR=kubespray-venv
+KUBESPRAYDIR=/opt/kubespray
+ANSIBLE_VERSION=2.12
+cd $KUBESPRAYDIR
+pip3.8 install -U -r requirements-$ANSIBLE_VERSION.txt --user
+test -f requirements-$ANSIBLE_VERSION.yml && \
+ansible-galaxy role install -r requirements-$ANSIBLE_VERSION.yml && \
+ansible-galaxy collection -r requirements-$ANSIBLE_VERSION.yml    
